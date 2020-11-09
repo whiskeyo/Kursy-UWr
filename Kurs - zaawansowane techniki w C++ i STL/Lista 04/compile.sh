c73753ae01b485e2944b9879a9b46eb13cd84114
whiskeyo <duchowe50k@gmail.com>
2020-11-09 18:17:23 +0100

    Add scripts for compiling and cleaning directory.

--- Kurs - zaawansowane techniki w C++ i STL/Lista 04/compile.sh
+++ Kurs - zaawansowane techniki w C++ i STL/Lista 04/compile.sh
@@ -1,5 +1,3 @@
 g++ zadanie01.cpp -o zadanie01.out -std=c++17 -Wall -Wextra -Werror -pedantic;
 g++ zadanie02.cpp -o zadanie02.out -std=c++17 -Wall -Wextra -Werror -pedantic;
 g++ zadanie03.cpp -o zadanie03.out -std=c++17 -Wall -Wextra -Werror -pedantic;
-g++ zadanie04.cpp -o zadanie04.out -std=c++17 -Wall -Wextra -Werror -pedantic;
-g++ zadanie05.cpp -o zadanie05.out -std=c++17 -Wall -Wextra -Werror -pedantic;
